ad2292a9d07df6f35a6ea4ca7d2216adf3f6f332
jswan <jswannieman@gmail.com>
2023-05-02 18:50:51 +1000

    Update NumberedList Javascript Component

--- src/NumberedList.js
+++ src/NumberedList.js
@@ -1,3 +1,14 @@
+import styled from "styled-components";
+
+const Heading3 = styled.h3`
+  text-align: center;
+  width: 2%;
+  height: 5%;
+  margin: auto;
+  border: 1px solid #000;
+  border-radius: 55%;
+`;
+
 const NumberedList = ({
   items,
   resourceName,
@@ -7,7 +18,7 @@ const NumberedList = ({
     <>
       {items.map((item, index) => (
         <>
-          <h3>{index + 1}</h3>
+          <Heading3>{index + 1}</Heading3>
           <ItemComponent key={index} {...{ [resourceName]: item }} />
         </>
       ))}
